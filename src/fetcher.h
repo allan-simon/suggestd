2829988fe980fee860e115fd007f0a01ed9c1714
Allan SIMON <allan.simon@supinfo.com>
2014-02-09 09:43:37 +0800

    initial commit: imported suggestd as it was for its version 0.9

---
+++ src/fetcher.h
@@ -0,0 +1,31 @@
+#ifndef _FETCHER_H
+#define _FETCHER_H
+
+#define FETCHER_SIZE 128
+
+#include <stdbool.h>
+
+typedef struct {
+	char *str;
+	int value;
+} FetcherItem;
+
+typedef struct {
+	int count;
+	FetcherItem items[FETCHER_SIZE];
+	int min;
+	int limit;
+	int stop;
+} Fetcher;
+
+Fetcher *fetcher_new();
+void fetcher_clear(Fetcher *this);
+void fetcher_free(Fetcher *this);
+void fetcher_add(Fetcher *this, char *str, int value);
+void fetcher_debug(Fetcher *this);
+void fetcher_sort(Fetcher *this);
+
+void fetcher_sorted_add(Fetcher *this, char *str, int value);
+void fetcher_sorted_finalize(Fetcher *this);
+
+#endif
